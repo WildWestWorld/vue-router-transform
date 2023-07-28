17dc1947a1f67557655afff1f78a43715f5174a7
ljk <380686356@qq.com>
2023-07-28 17:02:58 +0800

    图片居中 显示

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,13 +4,20 @@
     <div>
       <!-- 跳转到foo链接 -->
       <RouterLink class="m-3 text-sm btn" to="/foo"> Go </RouterLink>
+      <button btn @click="() => toggle()">Toggle</button>
     </div>
 
     <!-- 根据路由的修改  FloatProxy 将自己对应的样式信息 传给 FloatContainer -->
     <!-- FloatContainer带动里面的元素进行样式上的变化 -->
 
-    <FloatProxy class="w-50 h-50 rounded-xl"></FloatProxy>
+    <FloatProxy
+      :class="mode ? 'w-50 h-50' : 'w-60 h-30'"
+      rounded-xl
+    ></FloatProxy>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const mode = ref(false);
+const toggle = useToggle(mode);
+</script>
